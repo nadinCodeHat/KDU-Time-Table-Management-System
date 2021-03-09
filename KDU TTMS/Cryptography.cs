0e790811cd2f54ca62ce4b1eaccd560e0a9e10f3
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-03-09 19:44:17 +0530

    Created Cryptography.cs

---
+++ KDU TTMS/Cryptography.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDU_TTMS
+{
+    public class Cryptography
+    {
+
+        public static string Encrypt(string plainText)
+        {
+            return plainText;
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            return cipherText;
+        }
+
+    }
+}
