7bf61e6d6aefbd0d8a04f3bb492d77b53412c397
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-03-10 11:27:17 +0530

    Updated Add_Lecturer.cs

--- KDU TTMS/Add_Lecturer.cs
+++ KDU TTMS/Add_Lecturer.cs
@@ -16,5 +16,18 @@ namespace KDU_TTMS
         {
             InitializeComponent();
         }
+
+        //On addBtn click
+        //Insert lecturer login info
+        //string insertLoginQuery = "INSERT INTO Login_Info(email, password) VALUES (@email,@password)";
+        //using (SqlCommand cmd = new SqlCommand(insertLoginQuery, con))
+        //{
+        //    //Generate a random 8 characters password and encrypt
+        //    //@Todo send email with login_info
+        //    byte[] password = Cryptography.callEncrypt(Cryptography.generateRandom());
+        //    cmd.Parameters.AddWithValue("@email", emailTxt.Text.ToString());
+        //    cmd.Parameters.AddWithValue("@password", password);
+        //    cmd.ExecuteNonQuery();
+        //}
     }
 }
