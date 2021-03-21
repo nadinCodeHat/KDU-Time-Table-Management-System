d79a7f165a8f019c414b6129c05ef3391dabef25
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-03-21 17:31:52 +0530

    Updated Splash_Screen.cs

--- KDU TTMS/Splash_Screen.cs
+++ KDU TTMS/Splash_Screen.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +18,55 @@ namespace KDU_TTMS
             InitializeComponent();
         }
 
-        //TODO use async to load the splash screen
+        struct DataParameter
+        {
+            public int Process;
+            public int Delay;
+        }
+
+        private DataParameter _inputParameter;
+
+        private void Splash_Screen_Load(object sender, EventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+            {
+                _inputParameter.Delay = 1;
+                _inputParameter.Process = 1200;
+                backgroundWorker.RunWorkerAsync(_inputParameter);
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            guna2ProgressBar1.Value = e.ProgressPercentage;
+            guna2ProgressBar1.Update();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int process = ((DataParameter)e.Argument).Process;
+            int delay = ((DataParameter)e.Argument).Delay;
+            int index = 1;
+            try
+            {
+                for(int i = 0; i < process; i++)
+                {
+                    if (!backgroundWorker.CancellationPending)
+                    {
+                        backgroundWorker.ReportProgress(index++ * 100 / process, string.Format("Process data {0}", i));
+                        Thread.Sleep(delay);
+                    }
+                }
+            }catch(Exception ex)
+            {
+                backgroundWorker.CancelAsync();
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
     }
 }
